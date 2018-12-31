e2761693bc1e38c402846ee5cfb70619f9275abb
Gary <gary@rstudio.com>
2018-12-30 18:46:51 -0800

    sync woocommerce templates

--- wp-content/themes/Luxcentric/woocommerce/emails/customer-new-account.php
+++ wp-content/themes/Luxcentric/woocommerce/emails/customer-new-account.php
@@ -10,28 +10,28 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates/Emails
- * @version     1.6.4
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates/Emails
+ * @version 3.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 ?>
 
 <?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-	<p><?php printf( __( 'Welcome to your new account on %s. I am so glad you joined! <p>Your username is: <strong>%s</strong> or you can use your entire email address.', 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
+	<p><?php printf( esc_html__( 'Welcome to your new account on %1$s. I am so glad you joined! <p>Your username is: %2$s or you can use your entire email address.', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>' ); ?></p>
 
 <?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
 
-	<p><?php printf( __( 'Your password has been automatically generated: %s', 'woocommerce' ), '<strong>' . esc_html( $user_pass ) . '</strong>' ); ?></p>
+	<p><?php printf( esc_html__( 'Your password has been automatically generated: %s', 'woocommerce' ), '<strong>' . esc_html( $user_pass ) . '</strong>' ); ?></p>
 
 <?php endif; ?>
 
-	<p><?php printf( __( 'You can access your account, update your information and change your password here:  %s. <p>Take a look around and see all the fun things Luxcentric has available for you. <p>See you around the Internet,<br>Nicole', 'woocommerce' ), make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); ?></p>
+	<p><?php printf( esc_html__( 'You can access your account, update your information and change your password here:  %s. <p>Take a look around and see all the fun things Luxcentric has available for you. <p>See you around the Internet,<br>Nicole', 'woocommerce' ), make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); ?></p>
 
-<?php do_action( 'woocommerce_email_footer', $email );
+<?php
+do_action( 'woocommerce_email_footer', $email );
