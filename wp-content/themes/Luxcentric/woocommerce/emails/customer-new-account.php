77680d7bc8eb1e93b718e36d518fb774fa9b3dce
Gary Ritchie <gary@rstudio.com>
2019-11-24 15:57:44 -0800

    update woocommerce email templates
    - update two of them, add one that was missing (will update separately)

--- wp-content/themes/Luxcentric/woocommerce/emails/customer-new-account.php
+++ wp-content/themes/Luxcentric/woocommerce/emails/customer-new-account.php
@@ -12,26 +12,28 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates/Emails
- * @version 3.5.2
+ * @version 3.7.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+defined( 'ABSPATH' ) || exit;
 
-?>
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
-
-	<p><?php printf( esc_html__( 'Welcome to your new account on %1$s. I am so glad you joined! <p>Your username is: %2$s or you can use your entire email address.', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>' ); ?></p>
-
+<?php /* translators: %s Customer username */ ?>
+<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
+<?php /* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */ ?>
+<p><?php printf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>', make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 <?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
-
+	<?php /* translators: %s Auto generated password */ ?>
 	<p><?php printf( esc_html__( 'Your password has been automatically generated: %s', 'woocommerce' ), '<strong>' . esc_html( $user_pass ) . '</strong>' ); ?></p>
-
 <?php endif; ?>
 
-	<p><?php printf( esc_html__( 'You can access your account, update your information and change your password here:  %s. <p>Take a look around and see all the fun things Luxcentric has available for you. <p>See you around the Internet,<br>Nicole', 'woocommerce' ), make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); ?></p>
-
 <?php
+/**
+ * Show user-defined additional content - this is set in each email's settings.
+ */
+if ( $additional_content ) {
+	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+}
+
 do_action( 'woocommerce_email_footer', $email );
